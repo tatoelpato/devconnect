5b819f6594b5dea776bbd09d4cc4e640cb1db6a4
Ramiro E Amaya <tato714@yahoo.com>
2022-10-31 08:32:31 -0700

    fixed db connection, removed environment variable, hosting db on aws.

--- config/db.js
+++ config/db.js
@@ -1,21 +1,22 @@
-const Pool = require("pg").Pool;
-
-const pool = new Pool({
-    user: "postgres",
-    password: "postgres",
-    host: "database-1.cwkmd4pcunnw.us-east-1.rds.amazonaws.com",
-    port: 5432,
-    database: "reactdb",
-});
+require('dotenv').config();
+const { Pool, Client } = require('pg');
 
 const connectDB = async () => {
-    try {
-        await pool.connect();
-        console.log('DB connected.');
-    } catch (err) {
-        console.err(err.message);
-        process.exit(1)
-    }
-}
+  const pool = new Pool({
+    user: process.env.REACT_APP_DB_USER,
+    password: process.env.REACT_APP_PG_PASSWORD,
+    host: process.env.REACT_APP_PG_HOST,
+    port: process.env.REACT_APP_PG_PORT,
+    database: process.env.REACT_APP_PG_DATABASE
+  });
+
+  try {
+    await pool.connect();
+    console.log('DB connected.');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
